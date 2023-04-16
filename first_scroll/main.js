4e90e6f2e5de1584a8c9a49a62fc573ac6bfb2fb
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-16 14:29:31 +0200

    config: scene, camera, renderer

--- first_scroll/main.js
+++ first_scroll/main.js
@@ -11,3 +11,7 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
+
+camera.position.setZ(30);
+
+renderer.render( scene, camera )
