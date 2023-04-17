2e354495b0bf18703eb54b25f892a0cda76a70ad
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-17 14:32:58 +0200

    camra

--- journey/03-basic_scene/script.js
+++ journey/03-basic_scene/script.js
@@ -3,3 +3,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const camera = new THREE.PerspectiveCamera();
+scene.add(camera);
