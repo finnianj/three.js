c547cd2103de237f50fdea9afca1b4799680d728
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-17 14:29:56 +0200

    scene

--- journey/03-basic_scene/script.js
+++ journey/03-basic_scene/script.js
@@ -1,4 +1,5 @@
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
