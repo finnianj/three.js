0d29fe5fe5929a427b86dcc5f20e26b3efd243b2
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-17 15:47:24 +0200

    vector 3

--- journey/05-transform/src/script.js
+++ journey/05-transform/src/script.js
@@ -15,28 +15,32 @@ const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
 mesh.position.z = 1
+mesh.position.x = 1
 console.log(mesh.position.length())
 // because position inherits from Vector3, you can use the length method.
 // this tells you how far an object is from the centre
 
 /**
  * Sizes
- */
+*/
 const sizes = {
-    width: 800,
-    height: 600
+  width: 800,
+  height: 600
 }
 
 /**
  * Camera
- */
+*/
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
+console.log(mesh.position.distanceTo(camera.position))
+// calculating distance from one object to another
+
 /**
  * Renderer
- */
+*/
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
