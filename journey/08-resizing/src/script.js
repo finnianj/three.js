2502c74c37eadc17c85fd86843dec8885b029a56
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-19 13:35:48 +0200

    window fullscreen resizing safari compatability

--- journey/08-resizing/src/script.js
+++ journey/08-resizing/src/script.js
@@ -43,7 +43,7 @@ window.addEventListener('resize', () => {
 })
 
 window.addEventListener('dblclick', () => {
-  const fullscreenElement = document.fullscreenElement || document.webkit.FullscreenElement
+  const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
 
   if (!fullscreenElement) {
     if (canvas.requestFullscreen()) {
