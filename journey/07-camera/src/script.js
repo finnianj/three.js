635461cf0f77b01d8602d6ff2214b976fc804a3d
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-19 11:52:37 +0200

    import orbit controls

--- journey/07-camera/src/script.js
+++ journey/07-camera/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 const cursor = {
   x:0,
@@ -52,6 +53,8 @@ camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -71,10 +74,12 @@ const tick = () =>
     // Update camera
     // camera.position.x = cursor.x * 10
     // camera.position.y = cursor.y * 10
-    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    camera.position.y = cursor.y * 3
-    camera.lookAt(mesh.position)
+
+    // Move camera in a circle:
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 3
+    // camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
