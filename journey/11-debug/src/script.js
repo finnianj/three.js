959b6a827746fef2f87ac91e0e20f1c03a5664c9
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-21 17:21:46 +0200

    Adding mesh

--- journey/11-debug/src/script.js
+++ journey/11-debug/src/script.js
@@ -23,6 +23,8 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+// Debug
+lilgui.add(mesh.position, 'y')
 
 /**
  * Sizes
