cfac3eb7f70c3c7e8428b308cf7c865b6942f893
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-21 11:42:36 +0200

    specifying if tetxure which are too small should be blurred or pixelated

--- journey/10-textures/src/script.js
+++ journey/10-textures/src/script.js
@@ -28,7 +28,8 @@ loadingManager.onError = () => {
   console.log('on error');
 }
 const textureLoader = new THREE.TextureLoader(loadingManager)
-const colorTexture = textureLoader.load('/textures/checkerboard-8x8.png')
+const colorTexture = textureLoader.load('/textures/door/color.jpg')
+const minecraftTexture = textureLoader.load('/textures/minecraft.png')
 const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
 const heightTexture = textureLoader.load('/textures/door/height.jpg')
 const normalTexture = textureLoader.load('/textures/door/normal.jpg')
@@ -36,7 +37,11 @@ const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclus
 const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const roughnessnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
-colorTexture.magFilter = THREE.NearestFilter
+// Minecraft style effect
+// minecraftTexture.magFilter = THREE.NearestFilter
+
+
+
 /**
  * Base
  */
