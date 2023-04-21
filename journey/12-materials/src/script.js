488384d5c8f01950cd7cc114979e80f5052278e0
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-21 18:00:00 +0200

    Sphere and plane

--- journey/12-materials/src/script.js
+++ journey/12-materials/src/script.js
@@ -10,6 +10,23 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Objects
+
+const material = new THREE.MeshBasicMaterial();
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  material
+)
+sphere.position.x = - 1.5
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(1, 1),
+  material
+)
+scene.add(sphere, plane)
+
+
 /**
  * Sizes
  */
