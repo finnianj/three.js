7238c9e5d2b824dfea60ab62cda9b4e62fa4cba1
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-25 12:22:47 +0200

    blurring shadow using shadow radius

--- journey/16-shadows/src/script.js
+++ journey/16-shadows/src/script.js
@@ -23,7 +23,7 @@ gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0x000fff, 2)
+const directionalLight = new THREE.DirectionalLight(0xff5000, 2)
 directionalLight.position.set(2, 2, - 1)
 gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
@@ -39,8 +39,9 @@ directionalLight.shadow.camera.top = 2
 directionalLight.shadow.camera.right = 2
 directionalLight.shadow.camera.bottom = - 2
 directionalLight.shadow.camera.left = - 2
+directionalLight.shadow.radius = 10
 const dlHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
-scene.add(dlHelper)
+// scene.add(dlHelper)
 scene.add(directionalLight)
 console.log(directionalLight.shadow);
 
