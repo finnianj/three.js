861bce65a8a74adafc9fafb003f24a84156359f7
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-26 11:33:38 +0200

    implementing all bricks textures

--- journey/17-haunted_house/src/script.js
+++ journey/17-haunted_house/src/script.js
@@ -31,7 +31,10 @@ const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
 const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
-
+const bricksColorTexture = textureLoader.load('/textures/bricks/color.jpg')
+const bricksAOTexture = textureLoader.load('/textures/bricks/ambientOcclusion.jpg')
+const bricksNormalTexture = textureLoader.load('/textures/bricks/normal.jpg')
+const bricksRoughnessTexture = textureLoader.load('/textures/bricks/roughness.jpg')
 /**
  * House
  */
@@ -41,8 +44,14 @@ scene.add(house)
 // Walls
 const walls = new THREE.Mesh(
   new THREE.BoxGeometry(4, 2.5, 4),
-  new THREE.MeshStandardMaterial({ color: '#ac8e82'})
+  new THREE.MeshStandardMaterial({
+    map: bricksColorTexture,
+    aoMap: bricksAOTexture,
+    normalMap: bricksNormalTexture,
+    roughnessMap: bricksRoughnessTexture
+  })
 )
+walls.geometry.setAttribute('uv2', new THREE.Float32BufferAttribute(walls.geometry.attributes.uv.array, 2))
 walls.position.y = 1.25
 house.add(walls)
 
