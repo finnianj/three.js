170217a9b69fa4a914cdfe78293f3d14ad5ac574
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-26 10:41:54 +0200

    Bushes

--- journey/17-haunted_house/src/script.js
+++ journey/17-haunted_house/src/script.js
@@ -65,6 +65,12 @@ bush1.position.set(0.8, 0.2, 2.2)
 bush2.scale.set(0.25, 0.25, 0.25)
 bush2.position.set(1.4, 0.1, 2.1)
 
+bush3.scale.set(0.4, 0.4, 0.4)
+bush3.position.set(-0.8, 0.1, 2.2)
+
+bush4.scale.set(0.15, 0.15, 0.15)
+bush4.position.set(-1, 0.05, 2.6)
+
 house.add(bush1, bush2, bush3, bush4)
 
 // Floor
