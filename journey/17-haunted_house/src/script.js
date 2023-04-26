30481d79685ec8f41f15f86404bed3482e77417e
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-26 11:22:14 +0200

    Loading textures

--- journey/17-haunted_house/src/script.js
+++ journey/17-haunted_house/src/script.js
@@ -23,6 +23,14 @@ scene.fog = fog
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAOTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
 
 /**
  * House
