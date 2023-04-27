f5db62ad33eb4aa82544788e3102c7c5a1df1eb0
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-27 17:33:24 +0200

    Randomness

--- journey/20-galaxy/src/script.js
+++ journey/20-galaxy/src/script.js
@@ -20,7 +20,8 @@ const parameters = {
   size: 0.01,
   radius: 5,
   branches: 3,
-  spin: 1
+  spin: 1,
+  randomness: 0.2
 }
 
 let geometry = null;
@@ -49,18 +50,25 @@ const generateGalaxy = () => {
   for (let i = 0; i < parameters.count; i++) {
     const i3 = i * 3
     const radius = Math.random() * parameters.radius
+    const spinAngle = radius * parameters.spin
     // The angles of a circle are measured in radians
     // 2piRadians will always equal a full circle in radians (see wikipedia for radians)
     // The circle is divided into the number of branches. For each i value, you will get a radian.
     // i.e if branches is 3, then you will get radians equalling 1/3, 1/6 and one whole circle
     const radian = (i % parameters.branches) / parameters.branches * Math.PI * 2
 
+    const randomX = (Math.random() - 0.5) * parameters.randomness
+    const randomY = (Math.random() - 0.5) * parameters.randomness
+    const randomZ = (Math.random() - 0.5) * parameters.randomness
+
     // When you use sin and cos, you get a position on a circle, whose default radius is 1
     // We need to increase the radius
-    vertices[i3 + 0] = Math.cos(radian) * radius
-    vertices[i3 + 1] = 0
-    vertices[i3 + 2] = Math.sin(radian) * radius
+    vertices[i3 + 0] = Math.cos(radian + spinAngle) * radius + randomX
+    vertices[i3 + 1] = randomY
+    vertices[i3 + 2] = Math.sin(radian + spinAngle) * radius + randomZ
+
   }
+
   geometry.setAttribute(
     'position',
     new THREE.BufferAttribute(vertices, 3)
@@ -77,6 +85,7 @@ gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).name('Star size').on
 gui.add(parameters, 'radius').min(0.001).max(20).step(0.001).name('Galaxy Radius').onFinishChange(generateGalaxy)
 gui.add(parameters, 'branches').min(2).max(20).step(1).name('Galaxy Branches').onFinishChange(generateGalaxy)
 gui.add(parameters, 'spin').min(-5).max(5).step(0.1).name('Branch Spin').onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomness').min(0).max(2).step(0.01).name('Randomness').onFinishChange(generateGalaxy)
 
 // /**
 //  * Test cube
