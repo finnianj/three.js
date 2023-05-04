aaf121b792a429998ceba7907a81d36eee8736d3
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-05-04 12:11:16 +0200

    materials

--- journey/21-physics/src/script.js
+++ journey/21-physics/src/script.js
@@ -138,13 +138,23 @@ world.gravity.set(0, -9.82, 0)
 
 const concreteMaterial = new CANNON.Material('concrete')
 const plasticMaterial = new CANNON.Material('plastic')
+const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+  concreteMaterial,
+  plasticMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7
+  }
+)
+world.addContactMaterial(concretePlasticContactMaterial)
 
 // cannon sphere
 const sphereShape = new CANNON.Sphere(0.5)
 const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
-  shape: sphereShape
+  shape: sphereShape,
+  material: plasticMaterial
 })
 world.addBody(sphereBody)
 
@@ -153,10 +163,11 @@ const floorShape = new CANNON.Plane()
 const floorBody = new CANNON.Body()
 floorBody.mass = 0
 floorBody.addShape(floorShape)
-// floorBody.quaternion.setFromAxisAngle(
-//   new CANNON.Vec3(-1, 0, 0),
-//   Math.PI * 0.5
-// )
+floorBody.material = concreteMaterial
+floorBody.quaternion.setFromAxisAngle(
+  new CANNON.Vec3(-1, 0, 0),
+  Math.PI * 0.5
+)
 
 world.addBody(floorBody)
 
