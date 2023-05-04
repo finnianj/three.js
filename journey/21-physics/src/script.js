d28b3fbadfaf5a047434ad75b4ac6c4c7996fafa
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-05-04 11:23:37 +0200

    instantiating new cannon body

--- journey/21-physics/src/script.js
+++ journey/21-physics/src/script.js
@@ -120,6 +120,13 @@ controls.enableDamping = true
 // Physics
 
 const world = new CANNON.World()
+world.gravity.set(0, -9.82, 0)
+const sphereShape = new CANNON.Sphere(0.5)
+const sphereBody = new CANNON.Body({
+  mass: 1,
+  position: new CANNON.Vec3(0, 3, 0),
+  shape: sphereShape
+})
 
 /**
  * Renderer
