ca135eb4273d4dc9696c5fdfb9323aa60b91dc4a
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-05-05 10:33:17 +0200

    optimise using sleep

--- journey/21-physics/src/script.js
+++ journey/21-physics/src/script.js
@@ -151,6 +151,7 @@ controls.enableDamping = true
 
 const world = new CANNON.World()
 world.broadphase = new CANNON.SAPBroadphase(world)
+world.allowSleep = true
 world.gravity.set(0, -9.82, 0)
 
 // const concreteMaterial = new CANNON.Material('concrete')
