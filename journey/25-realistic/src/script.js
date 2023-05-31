9c5f354184092f1a846c3da8ee033db458da32fa
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-05-31 12:15:35 +0200

    env map

--- journey/25-realistic/src/script.js
+++ journey/25-realistic/src/script.js
@@ -62,6 +62,7 @@ const environmentMap = cubeTextureLoader.load([
   '/textures/environmentMaps/0/pz.jpg',
   '/textures/environmentMaps/0/nz.jpg',
 ])
+scene.background = environmentMap
 
 /**
  * Sizes
