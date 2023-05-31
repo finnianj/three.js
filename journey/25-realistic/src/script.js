3002575c38752091acecaa6576a596bccff254fa
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-05-31 12:04:48 +0200

    physically correct lights

--- journey/25-realistic/src/script.js
+++ journey/25-realistic/src/script.js
@@ -80,6 +80,7 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+renderer.physicallyCorrectLights = true;
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
