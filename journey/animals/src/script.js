81369e9c8acbb67977b71e253324d1e9321a8e34
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-06-19 12:05:28 +0200

    Resetting 22

---
+++ journey/animals/src/script.js
@@ -0,0 +1,218 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+
+const objectsToAnimate = []
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Draco loader
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
+
+// GLTF
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+// gltfLoader.load('/models/Duck/glTF/Duck.gltf',
+//   (gltf) => {
+//     console.log('success');
+//     const duck = gltf.scene.children[0]
+//     scene.add(duck)
+//     objectsToAnimate.push(duck)
+//   }
+// )
+// gltfLoader.load('/models/FlightHelmet/glTF/FlightHelmet.gltf',
+//   (gltf) => {
+//     console.log('success');
+//     const children = [...gltf.scene.children]
+//     for (const item of children) {
+//       console.log(item);
+//       item.position.z = 2
+//       item.rotation.y = Math.PI * 0.5
+//       scene.add(item)
+//       objectsToAnimate.push(item)
+//     }
+//   }
+// )
+
+let mixer = null
+
+let params = {
+  number: 5
+}
+
+gltfLoader.load('/models/animals/sparrow.glb',
+  (gltf) => {
+    console.log('success');
+    console.log(gltf.scene);
+
+    // const fox = gltf.scene
+    scene.add(gltf.scene)
+    // objectsToAnimate.push(fox)
+    gltfLoader.load('/models/animals/sparrow_animations.glb',
+    (anim) => {
+        console.log(anim);
+        console.log('success');
+        mixer = new THREE.AnimationMixer(gltf.scene)
+        params.mixer = mixer;
+        params.animations = anim.animations;
+      }
+      )
+    }
+)
+
+const playAction = () => {
+  console.log('New action');
+  params.mixer.stopAllAction()
+  // if (params.mixer.actions) params.mixer.uncacheAction(params.mixer.actions[0])
+  const action = params.mixer.clipAction(params.animations[params.number])
+  console.log(params.mixer);
+  action.play()
+}
+
+gui.add(params, 'number').min(0).max(17).step(1).onFinishChange(playAction)
+
+
+
+    // gltfLoader.load('/models/Fox/glTF/Fox.gltf',
+//   (gltf) => {
+//     console.log('success');
+//     console.log(gltf);
+
+//     mixer = new THREE.AnimationMixer(gltf.scene)
+//     const action = mixer.clipAction(gltf.animations[1])
+//     gltf.scene.scale.set(0.025, 0.025, 0.025)
+
+//     console.log(action);
+//     action.play()
+
+//     const fox = gltf.scene
+//     scene.add(fox)
+//     objectsToAnimate.push(fox)
+//   }
+// )
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+        color: '#444444',
+        metalness: 0,
+        roughness: 0.5
+    })
+)
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(2, 2, 2)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.target.set(0, 0.75, 0)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let previousTime = 0
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+    // objectsToAnimate.forEach((item) => {
+    //   item.rotation.x = Math.sin(elapsedTime) / 4
+    // })
+
+    // Update controls
+    controls.update()
+
+    // Update mixer
+    if (mixer != null) {
+      mixer.update(deltaTime)
+    }
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
