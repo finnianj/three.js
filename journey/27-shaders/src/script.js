22ff9896b09374822b5037d8a0f894b77c79cc30
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-12-15 12:07:55 +0100

    french flag waving

--- journey/27-shaders/src/script.js
+++ journey/27-shaders/src/script.js
@@ -21,6 +21,7 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const flagTexture = textureLoader.load('/textures/flag-french.jpg')
 
 /**
  * Test mesh
@@ -41,11 +42,21 @@ const material = new THREE.RawShaderMaterial({
     vertexShader: testVertexShader,
     fragmentShader: testFragmentShader,
     transparent: true,
+    uniforms: {
+      uFrequency: { value: new THREE.Vector2(10, 5) },
+      uTime: { value: 0 },
+      uColor: { value: new THREE.Color('orange') },
+      uTexture: { value: flagTexture }
+    }
 })
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
+mesh.scale.y = 2 / 3
 scene.add(mesh)
+
+gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX')
+gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY')
 
 /**
  * Sizes
@@ -99,6 +110,8 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
